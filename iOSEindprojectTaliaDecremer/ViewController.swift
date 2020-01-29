fee0cc3c5eae76c16cbd32e10bdf9c9f95759cf6
TaliaGD <taliadecremer@telenet.be>
2020-01-29 11:15:16 +0100

    Initial Commit

---
+++ iOSEindprojectTaliaDecremer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSEindprojectTaliaDecremer
+//
+//  Created by mobapp13 on 29/01/2020.
+//  Copyright © 2020 Ehb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
